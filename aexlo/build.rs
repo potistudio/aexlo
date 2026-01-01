eb01f91433259f1a33d505283f5e2c790aa28073
POTI <potistudio@proton.me>
2026-01-02 04:27:38 +0900

    remove: unused library

--- aexlo/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-	let dst = cmake::Config::new("unsafe")
-		.define("CMAKE_BUILD_TYPE", "Release")
-		.define("CMAKE_POSITION_INDEPENDENT_CODE", "ON")
-		.build();
-
-	println!("cargo:rustc-link-search=native={}", dst.display());
-	println!("cargo:rustc-link-lib=static=unsafe");
-}
